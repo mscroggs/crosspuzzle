b537772b4ed15466ff2b5ad1427dc5ca4617290b
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-03-13 21:49:36 +0000

    add script to set version numbers

---
+++ set_version_number.py
@@ -0,0 +1,52 @@
+"""Set version number."""
+
+import argparse
+import re
+
+parser = argparse.ArgumentParser(description="Set version number")
+parser.add_argument('version', metavar='version', nargs=1,
+                    default=None, help="Vertion number.")
+
+args = parser.parse_args()
+version = args.version[0]
+
+
+if not re.match(r"^[0-9]+\.[0-9]+\.[0-9]+$", version):
+    raise ValueError(f"{version} has incorrect format")
+
+# VERSION
+with open("VERSION", "w") as f:
+    f.write(version)
+
+# crosspuzzle.css
+with open("crosspuzzle.css") as f:
+    content = f.read()
+pre, post = content.split("Crosspuzzle v", 1)
+post = post.split(" ", 1)[1]
+new_content = pre + "Crosspuzzle v" + version + post
+with open("crosspuzzle.css", "w") as f:
+    f.write(new_content)
+
+# crosspuzzle.js
+with open("crosspuzzle.js") as f:
+    content = f.read()
+pre, post = content.split("Crosspuzzle v", 1)
+post = post.split(" ", 1)[1]
+new_content = pre + "Crosspuzzle v" + version + post
+with open("crosspuzzle.js", "w") as f:
+    f.write(new_content)
+
+# examples.html
+with open("examples.html") as f:
+    content = f.read()
+pre, post = content.split("Crosspuzzle v", 1)
+post = post.split(" ", 1)[1]
+new_content = pre + "Crosspuzzle v" + version + post
+pre, post = content.split("crosspuzzle.css?v=", 1)
+post = post.split("'", 1)[1]
+new_content = pre + "crosspuzzle.css?v=" + version + post
+pre, post = content.split("crosspuzzle.js?v=", 1)
+post = post.split("'", 1)[1]
+new_content = pre + "crosspuzzle.js?v=" + version + post
+with open("examples.html", "w") as f:
+    f.write(new_content)
