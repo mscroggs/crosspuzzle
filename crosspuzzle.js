d28f69f2c7be66cbfe5d369943351f4647f97ed6
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-03-15 16:26:45 +0000

    0.5.0

--- crosspuzzle.js
+++ crosspuzzle.js
@@ -1,5 +1,5 @@
 /***********************************/
-/* Crosspuzzle v0.4.0              */
+/* Crosspuzzle v0.5.0              */
 /*                                 */
 /* Created by Matthew Scroggs      */
 /* Released under MIT license      */
